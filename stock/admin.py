86ab01715a961041f1b67d2dfd18db9c1e87b5a9
Dildora <dildora.babaerova@gmail.com>
2025-06-13 14:51:05 +0300

    registered Warehouse and WarehouseItem in admin.py

--- stock/admin.py
+++ stock/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Item, Order, WarehouseItem
-
-# Register your models here.
-admin.site.register(Item)
+from .models import Item, Order, WarehouseItem, Warehouse
 
 admin.site.register(Order)
+admin.site.register(Warehouse)
+
+# Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('koodi', 'nimike', 'lisanimike')
+
 
 @admin.register(WarehouseItem)
 class WarehouseItemAdmin(admin.ModelAdmin):
