3354dfe6cd1e08c55dbcedfa00270be5ce3a58f0
Dildora <dildora.babaerova@gmail.com>
2025-06-18 13:15:55 +0300

    removed lisanimike from db.sqlite3

--- stock/models.py
+++ stock/models.py
@@ -12,6 +12,9 @@ class Item(models.Model):
     koodi = models.CharField(max_length=255, unique=True)
     nimike = models.CharField(max_length=255)
     
+    class Meta:
+        managed = True
+        
     def __str__(self):
         return f"{self.koodi} {self.nimike}"
 
