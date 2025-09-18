b38d0a1c3e78f948b1ba7f4e79d1511dbb79e873
jannatuovinen <51450@edu.raseko.fi>
2025-09-18 10:22:44 +0300

    Vahdettu mainiin

--- order_from_stock/settings.py
+++ order_from_stock/settings.py
@@ -30,10 +30,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b4_jp)*(s33x703jr&vh6*u5bd665ge+4h_%mptlya8yt8e@u)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+<<<<<<< Updated upstream
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost').split(',')
 
+=======
+DEBUG = True
+ALLOWED_HOSTS = []
+>>>>>>> Stashed changes
 
 
 # Application definition
